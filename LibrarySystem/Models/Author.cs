1af66be31feb3a4eed1d990920baef249215fddd
una-07 <esirkepulpan8@gmail.com>
2026-04-20 15:08:18 +0500

    Create Author.cs

---
+++ LibrarySystem/Models/Author.cs
@@ -0,0 +1,12 @@
+namespace LibrarySystem.Models
+{
+    public class Author
+    {
+        public string Name { get; set; }
+
+        public Author(string name)
+        {
+            Name = name;
+        }
+    }
+}
